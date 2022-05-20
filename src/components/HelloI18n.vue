e73c6480848e79e88ead86e10fc28d8c3cf11612
mohsen_graphyy <piri.mohsen@ut.ac.ir>
2022-05-20 04:51:09 +0430

    add setup files

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ $t("title") }}</h1>
+  <p :class="{'rtl' : $i18n.locale == 'fa'}">{{ $t("description") }}</p>
+  <LocaleSwitcher />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import LocaleSwitcher from "./LocaleSwitcher.vue";
+export default defineComponent({
+  name: 'HelloI18n',
+  components: { LocaleSwitcher }
+})
+</script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello i18n in SFC!"
+  }
+}
+</i18n>
+<style scoped>
+  .rtl {
+    direction: rtl;
+    font-size: 2rem;
+    
+  }
+</style>
